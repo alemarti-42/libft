1245aaf111a88f4069049186802b06fdb3eb3ebb
Alejandro Martinez alonso <alemarti@c2r9s2.42madrid.com>
2021-05-25 17:42:07 +0200

    split funciona

--- ft_split.c
+++ ft_split.c
@@ -6,50 +6,50 @@
 /*   By: alemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 20:19:09 by alemarti          #+#    #+#             */
-/*   Updated: 2021/05/23 22:09:21 by alemarti         ###   ########.fr       */
+/*   Updated: 2021/05/25 17:41:38 by alemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
 static int		count_words(const char *s, char c);
-static int		count_letters(const char *s, char c);
 
 char	**ft_split(const char *s, char c)
 {
-	char	**res;
-	//char	*aux;
-	int		i;
-	int		j;
-	int		len;
+	char		**res;
+	size_t		i;
+	size_t		j;
+	int			count;
 
-	res = (char **)malloc(count_words(s, c));
-	if (!res)
-		return (NULL);
-	if (!s)
-		return (NULL);
 	i = 0;
 	j = 0;
-	while (s[i])
+	count = 0;
+	if (!s)
+		return (NULL);
+	res = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!res)
+		return (NULL);
+	while (i < ft_strlen(s) && count < count_words(s, c))
 	{
-		while (s[i] == c && s[i])
+		while (s[i] == c)
 			i++;
-		if (s[i] && s[i] != c)
-		{
-			len = count_letters(&s[i], c);
-			res[j] = (char *)malloc(len + 1);
-			ft_strlcpy(res[j], &s[i], len + 1); 
-			//res[j] = ft_substr(&s[i], 0, len);
+		j = 0;
+		while (s[i + j] != c && s[i + j] && (i + j) < ft_strlen(s))
 			j++;
-			//i += len;
-		}
-		while (s[i] && s[i] != c)
-			i++;
+		if ((i + j) > ft_strlen(s))
+			break ;
+		res[count] = (char *)malloc(j + 1);
+		if (!res[count])
+			return (NULL);
+		res[count] = ft_substr(s, i, j);
+		i += j ;
+		count++;
 	}
+	res[count] = NULL;
 	return (res);
 }
 
-static int		count_words(const char *s, char c)
+static int	count_words(const char *s, char c)
 {
 	int	count;
 	int	is_space;
@@ -70,14 +70,4 @@ static int		count_words(const char *s, char c)
 		i++;
 	}
 	return (count);
-}
-
-static int		count_letters(const char *s, char c)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] && s[i] != c)
-		i++;
-	return (i);
 }
